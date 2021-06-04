37159dbd9edb7c04a91d2ac9acfe36363ac147fb
inooll <inooll@126.com>
2021-06-04 13:31:43 +0800

    添加获取图片Servlet

---
+++ src/main/java/controller/GetCImagesServlet.java
@@ -0,0 +1,30 @@
+package controller;
+
+import dao.CImagesDAO;
+import dao.CommodityDAO;
+import entity.Commodity;
+import entity.CommodityImages;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class GetCImagesServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        int cid = Integer.parseInt(request.getParameter("cid"));
+        Commodity c = new CommodityDAO().get(cid);
+        List<CommodityImages> ci = new CImagesDAO().list(c);
+        request.setAttribute("commodity", c);
+        request.setAttribute("CommodityImages", ci);
+        //这里写返回参数的jsp
+        request.getRequestDispatcher("testGet.jsp").forward(request,response);
+
+    }
+
+
+}
