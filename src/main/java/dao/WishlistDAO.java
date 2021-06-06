2d40901f6c92bcd75c78a5ba594fd45c9216b72a
Hyper18 <windhyperspace@gmail.com>
2021-06-06 16:18:26 +0800

    完成WishlistDAO

---
+++ src/main/java/dao/WishlistDAO.java
@@ -0,0 +1,80 @@
+package dao;
+
+import entity.Wishlist;
+import util.DBUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WishlistDAO {
+    DBUtil util = new DBUtil();
+
+    public boolean create(Wishlist wishlist, HttpServletRequest request) {
+        String sql = "insert into wishlist values(?, ?, ?)";
+        boolean result = false;
+        try (PreparedStatement ps = util.createStatement(sql, request)) {
+
+            ps.setInt(1, wishlist.getId());
+            ps.setInt(2, wishlist.getUid());
+            ps.setInt(3, wishlist.getCid());
+            ps.execute();
+
+            result = true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            util.close(request);
+        }
+
+        return result;
+    }
+
+    public List<Wishlist> retrieve(HttpServletRequest request) {
+        String sql = "SELECT * FROM wishlist";
+        List<Wishlist> list = new ArrayList<>();
+        try {
+            PreparedStatement ps = util.createStatement(sql, request);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                Wishlist wishlist = new Wishlist();
+                wishlist.setId((rs.getInt("id")));
+                wishlist.setUid(rs.getInt("uid"));
+                wishlist.setCid(rs.getInt("cid"));
+
+                list.add(wishlist);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            util.close(request);
+        }
+
+        return list;
+    }
+
+    public boolean delete(Wishlist wishlist, HttpServletRequest request) {
+        String sql = "delete from wishlist where id = " + wishlist.getId()
+                + " and uid = " + wishlist.getUid() + " and cid = "
+                + wishlist.getCid();
+        boolean result = false;
+        try (PreparedStatement ps = util.createStatement(sql, request)) {
+            ps.execute(sql);
+
+            result = true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            util.close(request);
+        }
+
+        return result;
+    }
+}
--- src/main/java/entity/Wishlist.java
+++ src/main/java/entity/Wishlist.java
@@ -1,18 +1,29 @@
 package entity;
 
-import java.util.List;
-
 public class Wishlist {
     /*
-    Definitions：愿望单id 用户id 商品列表
+    Definitions：愿望单所属用户id 卖家用户id 商品列表
      */
     private int id;
     private int uid;
-    List<Commodity> commodity;
+    private int cid;
 
     /*
-    Getters and Setters
+    Constructors
      */
+    public Wishlist() {
+    }
+
+    public Wishlist(int id, int uid, int cid) {
+        this.id = id;
+        this.uid = uid;
+        this.cid = cid;
+    }
+
+    /*
+        Getters and Setters
+    */
+
     public int getId() {
         return id;
     }
@@ -21,12 +32,19 @@ public class Wishlist {
         this.id = id;
     }
 
-
-    public List<Commodity> getProducts() {
-        return commodity;
+    public int getUid() {
+        return uid;
     }
 
-    public void setProducts(List<Commodity> commodity) {
-        this.commodity = commodity;
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
     }
 }
